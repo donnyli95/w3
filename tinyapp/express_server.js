7597ca4df6235c98c66d61c5c461cddc5669f179
donnyli95 <sz3li@uwaterloo.ca>
2021-04-13 01:08:02 +0000

    started registration page

--- tinyapp/express_server.js
+++ tinyapp/express_server.js
@@ -84,4 +84,9 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
-
+app.get("/register", (req, res) => {
+  const templateVars = { 
+    username: req.cookies["username"]
+   }
+  res.render("register", templateVars)
+});
