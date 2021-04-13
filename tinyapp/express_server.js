c6d8f8fa55db525f3a9b291f792d07897464039c
donnyli95 <sz3li@uwaterloo.ca>
2021-04-13 00:46:00 +0000

    added log in/out features

--- tinyapp/express_server.js
+++ tinyapp/express_server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+
+app.set("view engine", "ejs");
+
+
 const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const generateRandomString = require("./randomString");
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,7 +31,10 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -34,7 +43,11 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
+  const templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+   }
   res.render("urls_show", templateVars)
 });
 
@@ -47,18 +60,28 @@ app.post("/urls", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
-})
+});
 
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
-})
+});
 
 app.post("/urls/:shortURL/edit", (req, res) => {
   let shortURL = req.params.shortURL
   res.redirect(`/urls/${shortURL}`);
-})
+});
 
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body);
+  res.redirect(`/urls`);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
 
 
