0ad7011dc77e2e221491bdc33108ad83b234a04e
donnyli95 <sz3li@uwaterloo.ca>
2021-04-15 03:04:35 +0000

    error handling

--- tinyapp/express_server.js
+++ tinyapp/express_server.js
@@ -60,11 +60,14 @@ app.get("/urls", (req, res) => {
     };
     res.render("urls_index", templateVars);
   } else {
-    res.sendStatus(403);
+    const templateVars = {
+      errorMessage: "PLease Log In First :)",
+    }
+    res.status(403).render("urls_index", templateVars);
   }
 });
 
-//Create new URL
+//Goes to new URL creation page
 app.get("/urls/new", (req, res) => {
   if (Object.keys(req.session).length > 1) {
     const templateVars = {
@@ -80,40 +83,66 @@ app.get("/urls/new", (req, res) => {
 
 // Shows specific URL information
 app.get("/urls/:shortURL", (req, res) => {
-  if (Object.keys(req.session).length > 1) {
-    let userArray = Object.keys(urlsForUser(req.session["user_id"], urlDatabase));
-    if (userArray.includes(req.params.shortURL)) {
-      const templateVars = {
-        shortURL: req.params.shortURL,
-        longURL: urlDatabase[req.params.shortURL].longURL,
-        serverCookies: req.session,
-        displayName: users[req.session["user_id"]].email
-      };
-      res.render("urls_show", templateVars);
-    } else {
-      res.sendStatus(401);
-    }
+  let userArray = Object.keys(urlsForUser(req.session["user_id"], urlDatabase));
+
+  if (Object.keys(req.session).length > 1 && userArray.includes(req.params.shortURL)) {
+    const templateVars = {
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.shortURL].longURL,
+      serverCookies: req.session,
+      displayName: users[req.session["user_id"]].email
+    };
+    res.render("urls_show", templateVars);
   } else {
-    res.sendStatus(403);
+    const templateVars = {
+      errorMessage: "Seriously, log in first!",
+    }
+    res.status(403).render("urls_show", templateVars);
   }
 });
 
 //Redirect to actual url link when clicked
 app.get("/u/:shortURL", (req, res) => {
-  res.redirect(urlDatabase[req.params.shortURL].longURL);
+  if (Object.keys(req.session).length > 1) {
+    res.redirect(urlDatabase[req.params.shortURL].longURL);
+  } else {
+    const templateVars = {
+      errorMessage: "This is not yours",
+    }
+    res.status(403).render("errors", templateVars);
+  }
 });
 
 // Adding more urls
 app.post("/urls", (req, res) => {
-  if (Object.keys(req.session).length > 1) {
+  if (Object.keys(req.session).length > 1 && req.body.longURL.includes('http')) {
     let shortURL = generateRandomString();
     urlDatabase[shortURL] = {
       longURL: req.body.longURL,
       userID: req.session["user_id"]
     };
-    res.redirect("/urls");
+    res.redirect(`/urls/${shortURL}`);
   } else {
-    res.sendStatus(401);
+    const templateVars = {
+      errorMessage: "Did you include 'http'?",
+    }
+    res.status(403).render("errors", templateVars);
+  }
+});
+
+//Update long URL of tiny URL
+app.post("/urls/:shortURL", (req, res) => {
+  if (req.session["user_id"] && req.body.newURL.includes('http')) {
+    urlDatabase[req.params.shortURL] = {
+      longURL: req.body.newURL,
+      userID: req.session["user_id"]
+    };
+    res.redirect(`/urls`);
+  } else {
+    const templateVars = {
+      errorMessage: "This only works if you include 'http'",
+    }
+    res.status(403).render("errors", templateVars);
   }
 });
 
@@ -122,7 +151,10 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   if (req.session["user_id"]) {
     delete urlDatabase[req.params.shortURL];
   }
-  res.redirect("/urls");
+  const templateVars = {
+    errorMessage: "You can't delete what's not yours",
+  }
+  res.status(403).render("errors", templateVars);
 });
 
 // Redirects to Tiny URL info page
@@ -131,20 +163,15 @@ app.post("/urls/:shortURL/edit", (req, res) => {
     let shortURL = req.params.shortURL;
     res.redirect(`/urls/${shortURL}`);
   } else {
-    res.redirect("/urls");
+    const templateVars = {
+      errorMessage: "You can't edit what's not yours",
+    }
+    res.status(403).render("errors", templateVars);
   }
 });
 
-//Update long URL of tiny URL
-app.post("/urls/:shortURL", (req, res) => {
-  urlDatabase[req.params.shortURL] = {
-    longURL: req.body.newURL,
-    userID: req.session["user_id"]
-  };
-  res.redirect("/urls");
-});
 
-// Header buttons
+// Header buttons/links
 app.post("/registerHeader", (req, res) => {
   res.redirect("/register");
 });
@@ -153,39 +180,7 @@ app.post("/loginHeader", (req, res) => {
   res.redirect("/login");
 });
 
-// Register functions
-app.get("/register", (req, res) => {
-  if (Object.keys(req.session).length > 1) {
-    res.redirect("/urls");
-  } else {
-    const templateVars = {
-      serverCookies: req.session
-    };
-    res.render("register", templateVars);
-  }
-
-});
-
-app.post("/register", (req, res) => {
-  let randomID = generateRandomString();
-
-  if (req.body.email.length === 0 || req.body.psw.length === 0) {
-    res.sendStatus(400);
-  } else if (emailExists(req.body.email, users)) {
-    res.sendStatus(400);
-  } else {
-    users[randomID] = {
-      id: randomID,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.psw, 10)
-    };
-    req.session["user_id"] = randomID;
-    res.redirect("/urls");
-  }
-
-});
-
-//Login functions
+// Login Get Method
 app.get("/login", (req, res) => {
   if (Object.keys(req.session).length > 1) {
     res.redirect("/urls");
@@ -199,16 +194,64 @@ app.get("/login", (req, res) => {
 
 });
 
+// Register Get Method
+app.get("/register", (req, res) => {
+  if (Object.keys(req.session).length > 1) {
+    res.redirect("/urls");
+  } else {
+    const templateVars = {
+      serverCookies: req.session
+    };
+    res.render("register", templateVars);
+  }
+
+});
+
+//Login Post Method
 app.post("/login", (req, res) => {
   if (!emailExists(req.body.email, users)) {
-    res.sendStatus(404);
+    const templateVars = {
+      errorMessage: "Please register first!",
+    }
+    res.status(404).render("errors", templateVars);
   } else if (!passwordMatch(req.body.email, req.body.psw, users)) {
-    res.sendStatus(403);
+    const templateVars = {
+      errorMessage: "Wrong password :(",
+    }
+    res.status(404).render("errors", templateVars);
   } else {
     req.session["user_id"] = getID(req.body.email, users);
     res.redirect("/urls");
   }
 });
+
+//Register Post Method
+app.post("/register", (req, res) => {
+  let randomID = generateRandomString();
+
+  if (req.body.email.length === 0 || req.body.psw.length === 0) {
+    const templateVars = {
+      errorMessage: "Please Fill Out Both Fields",
+    }
+    res.status(400).render("errors", templateVars);
+  } else if (emailExists(req.body.email, users)) {
+    const templateVars = {
+      errorMessage: "You Already Have An Account",
+    }
+    res.status(400).render("errors", templateVars);
+  } else {
+    users[randomID] = {
+      id: randomID,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.psw, 10)
+    };
+    req.session["user_id"] = randomID;
+    res.redirect("/urls");
+  }
+
+});
+
+
 
 //Logout
 app.post("/logout", (req, res) => {
